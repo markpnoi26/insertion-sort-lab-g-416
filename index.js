93892f457092798c5e50d876a56a3ff742b0fbd1
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-01-15 05:59:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function findMinAndRemove(array){
-
+  let min = array[0]
+  array.forEach(element => {
+    if (element < min) {
+      min = element
+    } 
+  })
+  return min
 }
 
 function insertionSort(array){
-
+  
 }
